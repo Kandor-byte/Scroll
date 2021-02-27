d23780a0eeed9a5db816cafccb0b4a3ded093b1a
ShuxHop <kopaczandor8@gmail.com>
2021-02-27 20:13:14 +0200

    updated repo

---
+++ script.js
@@ -0,0 +1,16 @@
+const flightPath = {
+    curviness: 1.25,
+    autoRotate: true,
+    values: [
+        {x: 100, y: -20}
+    ]
+};
+
+const tween = new TimeLineLite();
+
+Tween.add(
+    TweenLite.to(".paper-plane", 1, {
+        bezier: flightPath,
+        ease: Power1.easeInOut
+    })
+);
